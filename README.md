095b198046688b44b48d34a43ece15c057886fec
Chet Corcos <ccorcos@gmail.com>
2017-05-03 22:40:49 -0700

    desktop size

--- README.md
+++ README.md
@@ -12,39 +12,21 @@ open http://localhost:8080/
 
 ## To Do
 
-- output midi
-
-
 - scale presets
   - preset button
   - persist stores to local stores
+
 - synth effects
   - knob
   - reverb + other effects
 
+- design for different screen sizes
 
 - hints (frequencies, letters, etc)
-- midi / osc output?
 
-- synth engine
-- design for iphone
 - rename to "Groove Salad" + favicon
 - Demo video
-
-- better performance when rotating pie?
 - keyboard slide offset and the note offset should be a constraint
 - rotating the pie should have a min and max just like the keyboard
 
----
-
-- keyboard play notes -- start all on for and map to numbers with label
-- persist scale store to localStorage
-- "select chord" block (maj5, minor7, etc.)
-- "play chord", "play arpegio" -- keyboard mode
-- "sequencer" block -- general purpose!
-
-- create blocks, wire them up, switches, keymaps, select active play block
-
-- "samples" block
-- better global layout
-- drag and drop root marker
+- three.js better performance?
--- src/stores/Size.ts
+++ src/stores/Size.ts
@@ -10,18 +10,26 @@ export class SizeStore {
   // keyboardViewableButtons = new Value(6)
 
   // iPhone 7
-  pieDiameter = new Value(200);
-  keyboardHeight = new Value(250);
-  keyboardButtonHeight = new Value(96)
-  keyboardButtonWidth = new Value(64)
+  // pieDiameter = new Value(200);
+  // keyboardHeight = new Value(250);
+  // keyboardButtonHeight = new Value(96)
+  // keyboardButtonWidth = new Value(64)
+  // keyboardButtonMargin = new Value(8)
+  // keyboardViewableButtons = new Value(6)
+
+  // Desktop
+  pieDiameter = new Value(250);
+  keyboardHeight = new Value(200);
+  keyboardButtonHeight = new Value(80)
+  keyboardButtonWidth = new Value(48)
   keyboardButtonMargin = new Value(8)
-  keyboardViewableButtons = new Value(6)
+  keyboardViewableButtons = new Value(12)
 
   keyboardButtonSize = new DerivedValue(() =>
     this.keyboardButtonWidth.get() + 2 * this.keyboardButtonMargin.get()
   )
   keyboardWidth = new DerivedValue(() =>
-    this.keyboardButtonSize.get() * 6
+    this.keyboardButtonSize.get() * this.keyboardViewableButtons.get()
   )
 }
 
